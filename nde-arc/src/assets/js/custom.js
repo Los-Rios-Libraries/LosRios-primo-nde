4099e1aa749542e2318f4bd9412ef47f99589627
jkarlsen <karlsej@scc.losrios.edu>
2025-12-18 15:05:21 -0800

    Add LibAnswers floating chat widget

--- nde-arc/src/assets/js/custom.js
+++ nde-arc/src/assets/js/custom.js
@@ -1,1 +1,15 @@
-console.log('Hello from custom.js');
+const libchatHash = '39df8b17e49bd4efbb4461f1831118b9';
+(() => { // load libchat
+		const noWidgetPattern = new RegExp(`https://caccl-lrccd.primo.exlibrisgroup.com/(delivery|researchAssistant)`);
+		const host = 'answers.library.losrios.edu';
+		const div = document.createElement('div');
+		div.id = `libchat_${libchatHash}`;
+		document.getElementsByTagName('body')[0].appendChild(div);
+		const scr = document.createElement('script');
+		scr.src = `https://${host}/load_chat.php?hash=${libchatHash}`;
+		setTimeout(() => {
+			if (noWidgetPattern.test(location.href) === false) { // don't include in Alma viewer or Primo AI tool
+				document.getElementsByTagName('body')[0].appendChild(scr);
+			}
+		}, 2000);
+	})();
--- nde-scc/src/assets/js/custom.js
+++ nde-scc/src/assets/js/custom.js
@@ -1,1 +1,15 @@
-console.log('Hello from custom.js');
+const libchatHash = 'c5c1abe3b80e070681d3fa045fc19bf4';
+(() => { // load libchat
+		const noWidgetPattern = new RegExp(`https://caccl-lrccd.primo.exlibrisgroup.com/(delivery|researchAssistant)`);
+		const host = 'answers.library.losrios.edu';
+		const div = document.createElement('div');
+		div.id = `libchat_${libchatHash}`;
+		document.getElementsByTagName('body')[0].appendChild(div);
+		const scr = document.createElement('script');
+		scr.src = `https://${host}/load_chat.php?hash=${libchatHash}`;
+		setTimeout(() => {
+			if (noWidgetPattern.test(location.href) === false) { // don't include in Alma viewer or Primo AI tool
+				document.getElementsByTagName('body')[0].appendChild(scr);
+			}
+		}, 2000);
+	})();
