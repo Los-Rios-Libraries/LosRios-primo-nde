055f482b2c1c19e34f087961f14cfd095d6b0438
jkarlsen <karlsej@scc.losrios.edu>
2026-01-21 13:27:21 -0800

    Simplify path to assets folder using webpack constant

--- nde-crc/src/app/homepage-logo/homepage-logo.component.ts
+++ nde-crc/src/app/homepage-logo/homepage-logo.component.ts
@@ -1,19 +1,20 @@
 import { Component, inject, computed } from '@angular/core';
 import { Store, createFeatureSelector } from '@ngrx/store';
-import { VIEW_CONSTANTS } from '../shared/constants/app.constants';
 
 const selectRouterState = createFeatureSelector<any>('routerState');
+// webpack path is needed for relative path to image
+declare const __webpack_public_path__: string;
 @Component({
   selector: 'custom-homepage-logo',
   standalone: true,
   templateUrl: './homepage-logo.component.html',
   styleUrl: './homepage-logo.component.css',
 })
+
 export class HomepageLogoComponent {
-  // current college is needed for path to image
-  college = VIEW_CONSTANTS.libraryAcronym.toUpperCase();
   private store = inject(Store);
   routerState = this.store.selectSignal(selectRouterState);
+  imageUrl = `${__webpack_public_path__}assets/images/onesearch-logo.png`;
   showLogo = computed(() => {
     // only show logo on home page
     const page = this.routerState().routerState;
