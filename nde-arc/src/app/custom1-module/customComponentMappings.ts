4aeaab6856573a94623c71cc079201235fa079c9
jkarlsen <karlsej@scc.losrios.edu>
2026-05-07 07:52:00 -0700

    Add custom ILL link component

--- nde-arc/src/app/custom1-module/customComponentMappings.ts
+++ nde-arc/src/app/custom1-module/customComponentMappings.ts
@@ -1,10 +1,12 @@
 import { HomepageLogoComponent } from '../homepage-logo/homepage-logo.component';
 import { ProblemReporterComponent } from '../problem-reporter/problem-reporter.component';
 import { FeeInfoComponent } from "../fee-info/fee-info.component";
+import { IllLinkComponent } from '../ill-link/ill-link.component';  
 
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
   ['nde-top-bar-before', HomepageLogoComponent],
   ['nde-search-result-item-container-bottom', ProblemReporterComponent],
-  ['nde-fines-bottom', FeeInfoComponent]
+  ['nde-fines-bottom', FeeInfoComponent],
+  ['nde-request-card-bottom', IllLinkComponent]
 ]);
---
+++ nde-arc/src/app/ill-link/ill-link.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@Component({
+  selector: 'custom-ill-link',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './ill-link.component.html',
+  styleUrl: './ill-link.component.css'
+})
+export class IllLinkComponent{
+    @Input() private hostComponent: any;
+
+  get newLink() {
+    if (this.hostComponent.request && /interlibrary loan/i.test(this.hostComponent.request.type))  {
+    const baseUrl = this.hostComponent.request['link-to-service'];
+    const currentURL = window.location.href;
+    const encodedURL = encodeURIComponent(currentURL);
+    return `${baseUrl}&url=${encodedURL}`;
+    } else {    
+    return '';
+    }
+
+  }
+  ngOnInit() {
+    console.log('hostComponent:', this.hostComponent);
+  }
+
+
+
+}
