375b9e69a1f93956af4119d487d71542f7425e74
jkarlsen <karlsej@scc.losrios.edu>
2026-05-20 10:27:39 -0700

    Add local notes component to mapping

--- nde-arc/src/app/custom1-module/customComponentMappings.ts
+++ nde-arc/src/app/custom1-module/customComponentMappings.ts
@@ -5,13 +5,19 @@ import { IllLinkComponent } from '../ill-link/ill-link.component';
 import { LocalCreatorsComponent } from '../local-creators/local-creators.component';
 import { WrapperSearchContainDropdownAfterComponent } from '../wrapper-search-contain-dropdown-after/wrapper-search-contain-dropdown-after.component';
 import { BrowseLandingPageComponent } from '../browse-landing-page/browse-landing-page.component';
+import { LocalNotesComponent } from '../local-notes/local-notes.component';
 
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
   ['nde-top-bar-before', HomepageLogoComponent],
   ['nde-search-result-item-container-bottom', ProblemReporterComponent],
+  ['nde-full-display-service-container-before', LocalNotesComponent],
   ['nde-record-availability-top', LocalCreatorsComponent],
   ['nde-fines-bottom', FeeInfoComponent],
+  ['nde-locations-container-after', LocalNotesComponent],
+  ['nde-full-display-details-after', LocalNotesComponent],
+  ['nde-get-it-before', LocalNotesComponent],
+  ['nde-view-it-after', LocalNotesComponent] ,
   ['nde-request-card-bottom', IllLinkComponent],
   ['nde-search-contain-dropdown-after', WrapperSearchContainDropdownAfterComponent],
   ['nde-browse-home-page-bottom', BrowseLandingPageComponent]
--- nde-scc/src/app/custom1-module/customComponentMappings.ts
+++ nde-scc/src/app/custom1-module/customComponentMappings.ts
@@ -2,6 +2,7 @@ import { HomepageLogoComponent } from '../homepage-logo/homepage-logo.component'
 import { ProblemReporterComponent } from '../problem-reporter/problem-reporter.component';
 import { FeeInfoComponent } from "../fee-info/fee-info.component";
 import { LocalCreatorsComponent } from '../local-creators/local-creators.component';
+import { LocalNotesComponent } from '../local-notes/local-notes.component';
 import { BrowseLandingPageComponent } from '../browse-landing-page/browse-landing-page.component';  
 import { IllLinkComponent } from '../ill-link/ill-link.component';  
 import { WrapperSearchContainDropdownAfterComponent } from '../wrapper-search-contain-dropdown-after/wrapper-search-contain-dropdown-after.component';
@@ -10,8 +11,13 @@ import { WrapperSearchContainDropdownAfterComponent } from '../wrapper-search-co
 export const selectorComponentMap = new Map<string, any>([
   ['nde-top-bar-before', HomepageLogoComponent],
   ['nde-search-result-item-container-bottom', ProblemReporterComponent],
-  ['nde-fines-bottom', FeeInfoComponent]
+  ['nde-search-result-item-container-bottom', WrapperSearchResultItemContainerBottomComponent],  ['nde-full-display-service-container-before', LocalNotesComponent],
   ['nde-record-availability-top', LocalCreatorsComponent],
+  ['nde-fines-bottom', FeeInfoComponent],
+  ['nde-locations-container-after', LocalNotesComponent],
+  ['nde-full-display-details-after', LocalNotesComponent],
+  ['nde-get-it-before', LocalNotesComponent],
+  ['nde-view-it-after', LocalNotesComponent] ,
   ['nde-request-card-bottom', IllLinkComponent],
   ['nde-search-contain-dropdown-after', WrapperSearchContainDropdownAfterComponent],
   ['nde-browse-home-page-bottom', BrowseLandingPageComponent],
