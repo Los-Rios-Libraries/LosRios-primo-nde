3b8dc52569db2adc9521c70bd9b59a9e97419d60
jkarlsen <karlsej@scc.losrios.edu>
2026-06-01 16:26:12 -0700

    Improve change detection

--- nde-scc/src/app/homepage-logo/homepage-logo.component.ts
+++ nde-scc/src/app/homepage-logo/homepage-logo.component.ts
@@ -1,18 +1,21 @@
-import { Component, inject, computed } from '@angular/core';
+import { Component, inject, computed, ChangeDetectionStrategy } from '@angular/core';
 import { Store, createFeatureSelector } from '@ngrx/store';
+import { PrimoRouterState } from '../shared/models/routerState.model';
 
-const selectRouterState = createFeatureSelector<any>('routerState');
-// webpack path is needed for relative path to image
+const selectRouterState = createFeatureSelector<PrimoRouterState>('routerState');
+
 declare const __webpack_public_path__: string;
+
 @Component({
   selector: 'custom-homepage-logo',
   standalone: true,
   templateUrl: './homepage-logo.component.html',
   styleUrl: './homepage-logo.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class HomepageLogoComponent {
   private store = inject(Store);
+  
   routerState = this.store.selectSignal(selectRouterState);
   imageUrl = `${__webpack_public_path__}assets/images/onesearch-logo.png`;
   
