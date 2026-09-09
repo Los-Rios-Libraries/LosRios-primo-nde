efd8772cae142b6e9e9166d0c37c870ae7cec194
jkarlsen <karlsej@scc.losrios.edu>
2026-09-09 09:45:36 -0700

    Switch collections widget from Grid list to cards
    Grid-list did not accommodate longer collection titles well. Cards are a more obvious choice. Also seems to make responsive layout easier.

--- nde-arc/src/app/homepage-collections-widget/homepage-collections-widget.component.ts
+++ nde-arc/src/app/homepage-collections-widget/homepage-collections-widget.component.ts
@@ -4,16 +4,16 @@ import { map } from 'rxjs'; // 1. Imported map operator
 import { CollectionItem } from '../shared/models/collection.model';
 import { CachedHttpService } from '../services/cached-http.service';
 import { ArrayUtilsService } from '../services/array-utils.service';
-import { MatGridListModule } from '@angular/material/grid-list'; 
 import { MatIconModule } from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { VIEW_CONSTANTS } from '../shared/constants/app.constants';
 
 @Component({
   selector: 'custom-homepage-collections-widget',
   standalone: true,
-  imports: [MatGridListModule, MatTooltipModule, MatIconModule, MatButtonModule],
+  imports: [ MatTooltipModule, MatIconModule, MatButtonModule, MatCardModule],
   providers: [CachedHttpService],
   templateUrl: './homepage-collections-widget.component.html',
   styleUrl: './homepage-collections-widget.component.css',
@@ -26,6 +26,8 @@ export class HomepageCollectionsWidgetComponent {
   thumbnailBaseURL = '/view/delivery/thumbnail/01CACCL_LRCCD/';
   collectionPage = `/discovery/collectionDiscovery?vid=01CACCL_LRCCD:${this.college.toUpperCase()}&collectionId=`;
   collectionRoot = VIEW_CONSTANTS.collectionRoot;
+ 
+
 
   collections = toSignal(
     this.cachedHttp.get<CollectionItem[]>({
@@ -39,21 +41,12 @@ export class HomepageCollectionsWidgetComponent {
     ),
     { initialValue: [] },
   );
-// responsive solution from https://stackoverflow.com/a/48499719/1903000
-  numInRow: number = 4; // Default to 4 columns for desktop
-  itemHeight: string = '4:3'; // Aspect ratio for thumbnails
+
   constructor() {  }
 
-  ngOnInit() {
-    if (window.innerWidth <= 512) {
-      this.numInRow = 1;
-    } else if (window.innerWidth > 512 && window.innerWidth <= 890) {
-      this.numInRow = 2;
-    } else {
-      this.numInRow = 4;
-    }
-    this.itemHeight = (window.innerWidth <= 768) ? '140' : '4:3'; // fixed height for mobile, 4:3 for desktop
-  }
-  
+goToCollection(collection: CollectionItem) {
+
+ location.href = this.collectionPage + collection.col_id;
+}
   
 }
