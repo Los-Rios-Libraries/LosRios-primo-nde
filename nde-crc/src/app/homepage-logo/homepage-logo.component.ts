e126063fc283b2f4dadc471f95314090d23186ee
jkarlsen <karlsej@scc.losrios.edu>
2026-09-11 07:57:56 -0700

    Change method for calling in files from /assets

--- nde-crc/src/app/homepage-logo/homepage-logo.component.ts
+++ nde-crc/src/app/homepage-logo/homepage-logo.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject, computed, ChangeDetectionStrategy } from '@angular/core';
 import { Store, createFeatureSelector } from '@ngrx/store';
 import { PrimoRouterState } from '../shared/models/routerState.model';
+import {AssetsPublicPathDirective} from '../services/assets-public-path.directive';
 
 const selectRouterState = createFeatureSelector<PrimoRouterState>('routerState');
-
-declare const __webpack_public_path__: string;
 
 @Component({
   selector: 'custom-homepage-logo',
   standalone: true,
+  imports: [AssetsPublicPathDirective],
   templateUrl: './homepage-logo.component.html',
   styleUrl: './homepage-logo.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -17,7 +17,7 @@ export class HomepageLogoComponent {
   private store = inject(Store);
   
   routerState = this.store.selectSignal(selectRouterState);
-  imageUrl = `${__webpack_public_path__}assets/images/onesearch-logo.png`;
+  imageUrl = 'assets/images/onesearch-logo.png';
   
   showLogo = computed(() => this.routerState()?.routerState === 'home');
 }
